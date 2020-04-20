f591a853bcdb31b5b33a1c024a38890b04b27dba
bulinayas <62295510+bulinayas@users.noreply.github.com>
2020-04-20 16:17:28 +0300

    Update Light.java

--- Light.java
+++ Light.java
@@ -8,7 +8,7 @@ public Light(double intensity,double x,double y,double z) {
 }
 
 public double ComputeLighting(Vector P, Vector N) {
-   double i = 0.1;
+   double i = 0.1;//"окружающее освещение"
  
    Vector L = new Vector(this.position.x - P.x,this.position.y - P.y,this.position.z - P.z);
      
